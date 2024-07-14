221f61140c3049d9a5470916754a66c66527de66
awais0047 <awaiszahid047@gmail.com>
2024-07-14 03:11:19 -0400

    New chnage

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -145,7 +145,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
-
 
 
 MEDIA_URL = '/media/'
